0ecb1ae5ace5ab87f8c2022e478544554199fc85
Anastasiia Makhynia <mah5129@gmail.com>
2023-11-14 18:03:03 +0200

    add grammar and parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,111 @@
-peg::parser!{
-    pub grammar list_parser() for str {
-      rule number() -> u32
-        = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
-  
-      pub rule list() -> Vec<u32>
-        = "[" l:(number() ** ",") "]" { l }
-    }
-  }
+use pest_derive::Parser;
+use pest::{Parser, Span};
+// use anyhow::anyhow;
+ 
+#[derive(Parser)]
+#[grammar = "./svg.pest"]
+pub struct SvgParser;
+
+#[derive(Debug)]
+pub enum SvgContent {
+    Circle(Vec<(String, String)>),
+    Rect(Vec<(String, String)>),
+    Line(Vec<(String, String)>),
+    Text(String),
+}
+
+pub fn parse_svg(input: &str) -> Result<Vec<SvgContent>, pest::error::Error<Rule>> {
+  let mut tokens = vec![];
+  let parse_result = SvgParser::parse(Rule::svg_file, input);
+
+  return match parse_result {
+      Ok(pairs) => {
+          let svg_file = pairs
+              .into_iter()
+              .find(|pair| pair.as_rule() == Rule::svg_file)
+              .ok_or_else(|| {
+                  pest::error::Error::<Rule>::new_from_span(
+                      pest::error::ErrorVariant::CustomError {
+                          message: "No SVG file found".to_string(),
+                      },
+                      Span::new(input, 0, 0).unwrap(),
+                  )
+              })?;
+
+          let svg_content = svg_file
+              .into_inner()
+              .find(|pair| pair.as_rule() == Rule::svg_content)
+              .ok_or_else(|| {
+                  pest::error::Error::<Rule>::new_from_span(
+                      pest::error::ErrorVariant::CustomError {
+                          message: "No SVG content found".to_string(),
+                      },
+                      Span::new(input, 0, 0).unwrap(),
+                  )
+              })?;
+
+          for inner_pair in svg_content.into_inner() {
+              match inner_pair.as_rule() {
+                  Rule::element => {
+                      let element = inner_pair.into_inner().next().unwrap();
+                      match element.as_rule() {
+                          Rule::circle => {
+                            let mut attributes: Vec<(String, String)> =Vec::new();
+                            for attributes_pair in element.into_inner() {
+                              match attributes_pair.as_rule() {
+                                Rule::attribute => {
+                                  let mut attributes_pair_iter = attributes_pair.clone().into_inner();
+                                  let attribute_name = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  let attribute_value = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  attributes.push((attribute_name.as_str().to_string(), attribute_value.as_str().to_string()));
+                                }
+                                _ => {}
+                              }
+                            }
+                            tokens.push(SvgContent::Circle(attributes));
+                          }
+                          Rule::rect => { 
+                            let mut attributes: Vec<(String, String)> =Vec::new();
+                            for attributes_pair in element.into_inner() {
+                              match attributes_pair.as_rule() {
+                                Rule::attribute => {
+                                  let mut attributes_pair_iter = attributes_pair.clone().into_inner();
+                                  let attribute_name = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  let attribute_value = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  attributes.push((attribute_name.as_str().to_string(), attribute_value.as_str().to_string()));
+                                }
+                                _ => {}
+                              }
+                            }
+                            tokens.push(SvgContent::Rect(attributes));
+                          }
+                          Rule::line => {
+                            let mut attributes: Vec<(String, String)> =Vec::new();
+                            for attributes_pair in element.into_inner() {
+                              match attributes_pair.as_rule() {
+                                Rule::attribute => {
+                                  let mut attributes_pair_iter = attributes_pair.clone().into_inner();
+                                  let attribute_name = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  let attribute_value = attributes_pair_iter.next().unwrap().as_str().to_string();
+                                  attributes.push((attribute_name.as_str().to_string(), attribute_value.as_str().to_string()));
+                                }
+                                _ => {}
+                              }
+                            }
+                            tokens.push(SvgContent::Line(attributes));
+                          }
+                          _ => {}
+                      }
+                  }
+                  Rule::text_content => {
+                      tokens.push(SvgContent::Text(inner_pair.as_str().to_string()));
+                  }
+                  _ => {}
+              }
+          }
+
+          Ok(tokens)
+      }
+      Err(error) => Err(error),
+  };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use anyhow::anyhow;
 use my_parser_kma_makhynia::*;
-  pub fn main() {
-     println!("{:?}",list_parser::list("[1,1,2,3,5,123328]"));
-  }
+fn main() {
+   // Приклад рядка для парсингу
+   let input = r#"<svg>
+           <circle cx="10" cy="10" r="5" />
+           <rect x="20" y="20" width="10" height="10" />
+           Some text content
+       </svg>
+   "#;
+
+   match parse_svg(input) {
+       Ok(result) => {
+           println!("Parsing successful! Result: {:#?}", result);
+       }
+       Err(error) => {
+           eprintln!("Error during parsing: {:?}", error);
+       }
+   }
+}
+
+
